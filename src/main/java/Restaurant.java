d35023df8e654809ca4c86aba9ec43b626a8a37b
VinmayiSwamy <vinmayiswamy5@gmai.com>
2022-02-09 21:50:15 +0530

    Part 3: Solution

--- src/main/java/Restaurant.java
+++ src/main/java/Restaurant.java
@@ -64,10 +64,11 @@ public class Restaurant {
         return name;
     }
 
-    public int getOrderValue(List<String> orderedItemNames){
+    //Adding ordervalue method
+    public int getOrderValue(List<String> orderedItems){
         int totalOrderValue = 0;
-        for (String orderedItemName : orderedItemNames) {
-            totalOrderValue += findItemByName(orderedItemName).getPrice();
+        for (String orderedItem : orderedItems) {
+            totalOrderValue += findItemByName(orderedItem).getPrice();
         }
         return totalOrderValue;
     }
